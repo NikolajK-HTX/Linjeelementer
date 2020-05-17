b18f3aab7b6c7bbdc47d1823179d5abeb4b5e6bb
Nikolaj J-K <kagenogmig@gmail.com>
2020-05-17 09:49:43 +0200

    Ny feature + fix - Tryk for at vise punkt, hvor hældningen til punktet vises. - Inputfeltet udvider sig med klassen: 'uk-width-expand'.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let coordinateSystemSize = 20;
 let yCoordinateSize = 0;
 let arrowDensity = 1;
-let arrowStep = coordinateSystemSize/20 * (1 / arrowDensity);
+let arrowStep = coordinateSystemSize / 20 * (1 / arrowDensity);
 
 document.querySelector('#coordinateSizeSlider').addEventListener('input', updateGUI);
 document.querySelector('#densitySlider').addEventListener('input', updateGUI);
@@ -15,21 +15,25 @@ function setup() {
   createNewLineElements();
 }
 
+function mousePressed() {
+  drawFunction();
+}
+
 function drawGrid() {
   push();
   stroke(0);
   strokeWeight(1);
   line(0, height / 2, width, height / 2);
   line(width / 2, 0, width / 2, height);
-  text(coordinateSystemSize, width-15, height/2+12);
-  text(coordinateSystemSize, 5, height/2+12);
+  text(coordinateSystemSize, width - 15, height / 2 + 12);
+  text(coordinateSystemSize, 5, height / 2 + 12);
   pop();
 }
 
 function drawArrows() {
   push();
-  translate(width/2, height/2);
-  yCoordinateSize = (height/width) * coordinateSystemSize;
+  translate(width / 2, height / 2);
+  yCoordinateSize = (height / width) * coordinateSystemSize;
   for (let x = -coordinateSystemSize; x <= coordinateSystemSize; x += arrowStep) {
     for (let y = -yCoordinateSize; y <= yCoordinateSize; y += arrowStep) {
       let inputFromUser = document.querySelector('#derivateInput').value;
@@ -46,7 +50,7 @@ function createNewLineElements() {
   background(220);
 
   readInput();
-  
+
   drawGrid();
   drawArrows();
 }
@@ -57,10 +61,10 @@ function readInput() {
   document.querySelector('#coorVal').innerHTML = coordinateSizeSlider.value;
 
   let densitySlider = document.querySelector('#densitySlider');
-  arrowDensity = densitySlider.value/100;
+  arrowDensity = densitySlider.value / 100;
   document.querySelector('#densVal').innerHTML = densitySlider.value;
 
-  arrowStep = coordinateSystemSize/20 * (1 / arrowDensity);
+  arrowStep = coordinateSystemSize / 20 * (1 / arrowDensity);
 }
 
 function updateGUI() {
@@ -69,6 +73,42 @@ function updateGUI() {
 
   let densitySlider = document.querySelector('#densitySlider');
   document.querySelector('#densVal').innerHTML = densitySlider.value;
+
+}
+
+function drawFunction() {
+  let fPoints = [];
+
+  // calculate the points in the function
+  // draw a line between all the points
+  // draw the point at which is clicked
+  push();
+  strokeWeight(12);
+  stroke(0, 120);
+  point(mouseX, mouseY);
+
+  stroke(random() * 255, random() * 255, random() * 255, 100);
+  strokeWeight(10);
+  point(mouseX, mouseY);
+  
+  strokeWeight(2);
+  stroke(255);
+  textAlign(CENTER);
+  fill(0);
+  // udregn hældning
+  // dette er dumt lavet - hvis man ændrer differentialligningen og 
+  // laver et punkt er det for den nye differentialligning, mens den
+  // gamle stadig vises indtil man trykker INDLÆS igen
+  let x = Math.round(((mouseX-width/2) * (coordinateSystemSize*2 / width))*100)/100;
+  let y = Math.round(((height-mouseY)-height/2) * (yCoordinateSize*2 / height)*100)/100;
+  let inputFromUser = document.querySelector('#derivateInput').value;
+  if (inputFromUser == "") inputFromUser = "x/y";
+  let haldning = eval(inputFromUser);
+  haldning = Math.round(haldning*100)/100;
+  text(`x: ${x}`, mouseX, mouseY-30);
+  text(`y: ${y}`, mouseX, mouseY-20);
+  text(`Hældning: ${haldning}`, mouseX, mouseY - 10);
+  pop();
 
 }
 
@@ -81,7 +121,7 @@ class Arrow {
 
   display() {
     push();
-    translate(this.x * width/(coordinateSystemSize*2), -this.y * height/(yCoordinateSize*2));
+    translate(this.x * width / (coordinateSystemSize * 2), -this.y * height / (yCoordinateSize * 2));
     let angle = atan(-this.a);
     rotate(angle);
     rectMode(CENTER);
